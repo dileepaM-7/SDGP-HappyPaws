d29af2f5de3589a158fb241606b3b1d955ff6583
shan1226 <dlpmalshan@gmail.com>
2024-01-26 20:56:11 +0530

    Added the contactUs page

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './MainContent.css'
 import doggy_Image from '../../assets/doggy.png'
+import dog1 from '../../assets/dogImg1.jpg'
+import dog2 from '../../assets/dogImg2.jpg'
+import dog3 from '../../assets/dogImg3.jpg'
 
 const MainContent = () => {
   return (
     <div className='main_content'>
+      <img className='dog_Image1' src={dog1} alt="" /> 
+      <img className='dog_Image2' src={dog2} alt="" /> 
+      <img className='dog_Image3' src={dog3} alt="" /> 
       <img className='dogImage' src={doggy_Image} alt="" />
     </div>
   )
--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import './Contact.css';
 
-
 export const Contact = () => {
   return (
     <div className="contact-us-container">
+      
       <h2>Contact Us</h2>
-      <p>
+      <p> <br />
         If you have any questions or inquiries, please feel free to reach out to
         us using the form below.
       </p>
       <form className="contact-form">
-        <label htmlFor="name">Your Name:</label>
+        <label className = "name-input" htmlFor="name">Your Name:</label>
         <input type="text" id="name" name="name" required />
 
         <label htmlFor="email">Your Email:</label>
         <input type="email" id="email" name="email" required />
 
+        <label htmlFor="phone">Your Phone NO:</label>
+        <input type="phone" id="phone" name="phone" required />
+
         <label htmlFor="message">Your Message:</label>
         <textarea id="message" name="message" rows="4" required></textarea>
 
-        <button type="submit">Submit</button>
+        <div className="btn-container">
+          <button className="reset-btn" type="reset">Reset</button>
+          <button className="submit-btn" type="submit">Submit</button>
+        </div>
       </form>
     </div>
   );
