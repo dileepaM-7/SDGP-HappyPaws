c28bdb39bf228c1bbc4349d9570da1dccd022ff0
shan1226 <dlpmalshan@gmail.com>
2024-02-07 21:44:31 +0530

    Changed the regsiter forum

--- src/components/pages/Register/Register.jsx
+++ src/components/pages/Register/Register.jsx
@@ -4,9 +4,45 @@ import { Navbar } from '../../NavigationBar/Navbar'
 import MainContent from '../../MainContent/MainContent'
 const Register = () => {
   return (
-    <div className='register-content'>
+    <div >
       <Navbar/>
-      <MainContent/>
+      <MainContent className="main-content" />
+      <div className="form-content">
+        <form >
+          <label className='Name-lbl'>
+          Username :
+          <input
+              type="text"
+              required
+              className='input-field'
+            />
+          </label>
+          <label className='email-lbl'>
+          Email :
+          <input
+              type="text"
+              required
+              className='input-field'
+            />
+          </label>
+          <label className='password-lbl'>
+          Password :
+          <input
+              type="text"
+              required
+              className='input-field'
+            />
+          </label>
+          <label className='confirmPassword-lbl'>
+          Confirm Password :
+          <input
+              type="text"
+              required
+              className='input-field'
+            />
+          </label>
+        </form>
+      </div>
       
     </div>
   )
