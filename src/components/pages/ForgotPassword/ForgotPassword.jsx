70bef21ebaa578d9cde77035d06e6d8389b73547
shan1226 <dlpmalshan@gmail.com>
2024-02-09 11:36:51 +0530

    finalized the login forgot and register pagess

--- src/components/pages/ForgotPassword/ForgotPassword.jsx
+++ src/components/pages/ForgotPassword/ForgotPassword.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './ForgotPassword.css'
 import { Navbar } from '../../NavigationBar/Navbar'
 import MainContent from '../../MainContent/MainContent'
+import backbtn from '../../../assets/back.png'
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 
 const ForgotPassword = () => {
   return (
@@ -13,6 +15,22 @@ const ForgotPassword = () => {
         <p>An email with a verification link will be sent shortly,<br />
            Try to reset your password Using that.</p>
       </div>
+      <Link to="/Login">
+      <img src={backbtn} alt="" className='back-img'/>
+      </Link>
+      
+      <form className="reset-content">
+      <label className='resetPw-lbl'>
+            Email :
+            <br />
+            <input
+              type="text"
+              required
+              className='input-field'
+            />
+          </label>
+          <button type="submit" className='reset-btn'>Send</button>
+      </form>
     </div>
   )
 }
