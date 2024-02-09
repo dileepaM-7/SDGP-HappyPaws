6fbd2d7bd27471028767af56d09a1618d78e7571
shan1226 <dlpmalshan@gmail.com>
2024-02-09 09:30:52 +0530

    Created the forgot password pages

--- src/components/pages/ForgotPassword/ForgotPassword.jsx
+++ src/components/pages/ForgotPassword/ForgotPassword.jsx
@@ -8,7 +8,11 @@ const ForgotPassword = () => {
     <div className=''>
       <Navbar/>
       <MainContent/>
-      
+      <div className="forgotPw-content">
+        <h3>Reset Password</h3>
+        <p>An email with a verification link will be sent shortly,<br />
+           Try to reset your password Using that.</p>
+      </div>
     </div>
   )
 }
