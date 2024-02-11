52acd1225d40164cf967d206ef6f08a2b13a995a
shan1226 <dlpmalshan@gmail.com>
2024-02-11 10:48:40 +0530

    Added Some animations to the main page

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,27 +1,47 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom";
 import './Home.css';
 import play_Image from '../../../assets/play.png';
 import MainContent from "../../MainContent/MainContent";
 import { Navbar } from "../../NavigationBar/Navbar";
 import Footer from "../../Footer/Footer";
+import styled, { keyframes } from "styled-components";
+
+const textFade = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-200px);
+  } 
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+`;
+
+const Title = styled.div`
+  animation-name: ${textFade};
+  animation-duration: 1s;
+`;
 
 export const Home = () => {
   return (
     <div>
-      <Navbar/>
-      <MainContent/>
+      <Navbar />
+      <MainContent />
       <div className='text'>
-        <span className='welcome_txt'>Welcome... <br /><br /></span>
-        <span className='first_content' >Welcome to a <br /> world of care for </span>
-        <span className='second_content'>your <br />beloved pets <br /></span>
-        <span className='last_txt'>Discover expert pet care tips and advice to ensure your furry <br />
-          friend's well-being and happiness at every stage of their life.</span>
-      <Link to="/services" className='features-button'>
-        Our Services <img src={play_Image} alt="" className="playImage"/>
-      </Link>
+        {/* Applying the Title styled component to the entire content */}
+        <Title>
+          <span className='welcome_txt'>Welcome... <br /><br /></span>
+          <span className='first_content' >Welcome to a <br /> world of care for </span>
+          <span className='second_content'>your <br />beloved pets <br /></span>
+          <span className='last_txt'>Discover expert pet care tips and advice to ensure your furry <br />
+            friend's well-being and happiness at every stage of their life.</span>
+          <Link to="/services" className='features-button'>
+            Our Services <img src={play_Image} alt="" className="playImage" />
+          </Link>
+        </Title>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
