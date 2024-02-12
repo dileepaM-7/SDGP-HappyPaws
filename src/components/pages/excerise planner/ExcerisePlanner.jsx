0ef56cefd4ee91d39aa37f1a994832abe3de44bb
shan1226 <dlpmalshan@gmail.com>
2024-02-12 19:07:11 +0530

    Added the exceise palnner page navigation

---
+++ src/components/pages/excerise planner/ExcerisePlanner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ExcerisePlanner.css'
+import { Navbar } from '../../NavigationBar/Navbar'
+
+const ExcerisePlanner = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default ExcerisePlanner
