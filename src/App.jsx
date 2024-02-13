a9fc382e6f980277291deecf2288f298d5a0f86c
shan1226 <dlpmalshan@gmail.com>
2024-02-13 16:11:01 +0530

    Added the excerise planner input page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-// import { Navbar } from "./components/NavigationBar/Navbar";
+import { Navbar } from "./components/NavigationBar/Navbar";
 import { About, Contact, Home, Services } from "./components/pages";
 import Login from "./components/pages/Login/Login";
 import Register from "./components/pages/Register/Register";
 import ForgotPassword from "./components/pages/ForgotPassword/ForgotPassword";
 import Profile from "./components/pages/profile/Profile";
 import ExcerisePlanner from "./components/pages/excerise planner/ExcerisePlanner";
+import InputData from "./components/pages/excerise planner/inputData/InputData";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/ForgotPassword" element={<ForgotPassword/>}/>
         <Route path="/profile" element={<Profile/>}/>
         <Route path="/exceisePlanner" element={<ExcerisePlanner/>}/>
+        <Route path="/excerseInput" element={<InputData/>}/>
       </Routes>
     </div>
   );
