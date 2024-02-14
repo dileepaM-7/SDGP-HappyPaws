7872c349983cf6fa5cb776906184dfa83f00c3b4
shan1226 <dlpmalshan@gmail.com>
2024-02-14 22:10:03 +0530

    Created the features section and finazlied it

--- src/components/pages/Services/Services.jsx
+++ src/components/pages/Services/Services.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Navbar } from "../../NavigationBar/Navbar";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
-import Footer from "../../Footer/Footer";
-import food from "../../../assets/food.png"
-import ex from "../../../assets/ex.png"
+import food from "../../../assets/food.png";
+import ex from "../../../assets/ex.png";
+import consultant from "../../../assets/consultant.png";
+import vaccine from "../../../assets/vaccine.png";
+import medical from "../../../assets/medical.png";
+
 
 import './Services.css'
 import { HiArrowCircleRight } from "react-icons/hi";
@@ -25,13 +27,30 @@ export const Services = () => {
       <button className="ex-btn">Get Started <HiArrowCircleRight/></button>
       </div>
       <div className="consultant-search">
-        consultant search
+      <img src={consultant} alt="" className="consultant-image"/>
+        <p className="find-consultants-para">"Discover our new "Pet Consultant Search" feature! 
+        Easily find and connect <br />with experienced pet consultants who can provide personalized care 
+        tips <br />and guidance. Simply search, explore profiles, and book appointments for <br />tailored advice.
+        Your pet's well-being is just a click away!"</p>
+      <button className="consultant-btn">Get Started <HiArrowCircleRight/></button>
       </div>
       <div className="vaccine-date">
-        vaccine date
+      <img src={vaccine} alt="" className="vaccine-image"/>
+      <p>"Keep your pet's health in check effortlessly! Set personalized vaccination <br />
+       reminders, and we'll notify you on the exact date. Stay on top of your <br />furry 
+       friend's well-being with automatic alerts and a handy vaccine <br /> history tracker. 
+       Easy, convenient, and worry-free pet care at your fingertips!"</p>
+      <button className="vaccine-btn">Get Started <HiArrowCircleRight/></button>
       </div>
       <div className="medical-data">
-        medical data
+      <img src={medical} alt="" className="medical-image"/>
+      <p>
+      "Safeguard your pet's health history with our "Pet Medical Records" feature. <br />
+      Easily store and access vaccination details, treatments, and appointments <br />
+      in one place. Keep track of your pet's well-being with this simple <br />
+      and organized medical record feature!"
+      </p>
+      <button className="medical-btn">Get Started <HiArrowCircleRight/></button>
       </div>
     </div>
   );
