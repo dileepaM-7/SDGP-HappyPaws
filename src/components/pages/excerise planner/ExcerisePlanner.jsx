c6b0960537bdf5573bc3d545007b01fbc0205c76
Ishini Ranasinghe <ishiniranasinghe16@gmail.com>
2024-02-14 23:00:49 +0530

    new updated exercise Page

--- src/components/pages/excerise planner/ExcerisePlanner.jsx
+++ src/components/pages/excerise planner/ExcerisePlanner.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import './ExcerisePlanner.css'
 import { Navbar } from '../../NavigationBar/Navbar'
 import image1 from "../../../assets/exercise1.png";
+<<<<<<< Updated upstream
 
+=======
+import { Link } from 'react-router-dom';
+import petExercise from '../../../assets/exercisepage1.jpg'
+>>>>>>> Stashed changes
 
 
 const ExcerisePlanner = () => {
   return (
-    <div>
+    <div className='all'>
       <Navbar/>
 
       <div className='text1'>
@@ -19,10 +24,49 @@ const ExcerisePlanner = () => {
         Our programs, which include enjoyable games and engaging exercises, are intended to enhance <br/> your pet's physical condition, 
         provide and deepen your relationship with your pet. Let us go on <br/> a path towards a better, happier life together!</span>
         <img src={image1} alt="" className='image' />
+<<<<<<< Updated upstream
         <button className='get-started'>Get Started</button>
+=======
+        <Link to="/excerseInput">
+        <button className='get-started'>Get Started  </button>
+        </Link>
+        
+>>>>>>> Stashed changes
+      </div>
+      <div className='bottom'>
+        <h1 className='topic'>Find a Safe Exercise Routine for Your Pet</h1>
+        
+        <div className='form1'>
+        <br/>
+        <h1>We Love Your pets</h1>
+        <h5>Please enter the details for  get exercise plan</h5>
+        <form action="" >
+        <br/><br/>
+          <input type="text" name="breed" id="" placeholder='Pet Breed'
+          className='breed' />
+          <br/>
+          <input type="text"
+          name='Weight'
+          placeholder='Pet Weight'
+          className='Weight' />
+          <br/>
+          <input type="text"
+          name='Age' 
+          placeholder='Age'
+          className='Age'/>
+        <Link to="">
+        <button className='submit'>submit</button>
+        </Link>
+
+
+        </form>
+        
+         
+        </div>
+       
       </div>
       
-      
+      <img src={petExercise} alt=""  className='image2'/>
 
     </div>
    
