c9df6ae63f38c2a44f4f8418feee8f14bd5e9b95
Ishini Ranasinghe <ishiniranasinghe16@gmail.com>
2024-02-14 23:01:00 +0530

    new updated exercise Page

--- src/components/pages/excerise planner/inputData/inputData.jsx
+++ src/components/pages/excerise planner/inputData/inputData.jsx
@@ -1,1 +1,52 @@
+<<<<<<< Updated upstream
+=======
+import React from 'react'
+import './inputData.css'
+import { Navbar } from '../../../NavigationBar/Navbar'
+import petName from '../../../../assets/petName.png'
+
+const InputData = () => {
+  return (
+    <div className='all'>
+      <Navbar/>
+      <h1>Find a Safe Exercise Routine for Your Pet</h1>
+      
+            <fieldset className='set1'> 
+            <form action="#" method="get">  
+            <img src={petName} alt="" /> 
+                    <input 
+                        type="text"
+                        name="firstname"
+                        id="firstname"
+                        placeholder="Enter pet Name"
+                        required 
+                    />              
+              </form>
+              
+              
+              <label for="Age" ></label> 
+                    <br /> 
+                    <input type="radio" name="age"
+                        value="" id="0-10" /> 0-10 
+                    
+                    <input type="radio" name="age"
+                        value="" id="female" /> 
+                    10-15 
+                    <input type="radio" name="age"
+                        value="" id="other" /> 
+                    15-20
+
+
+                    </fieldset>
+              
+                
+                    
+
+
+
+
+    </div>
+  )
+}
+>>>>>>> Stashed changes
 
