9a9cdebfa63ca07d4bbbf2d0aad2f9e3a1c0c343
shan1226 <dlpmalshan@gmail.com>
2024-02-24 21:34:30 +0530

    updated some responsive pages

--- src/components/pages/Services/Services.jsx
+++ src/components/pages/Services/Services.jsx
@@ -16,14 +16,14 @@ export const Services = () => {
     <div id="services">
       <div className="meal-suggestion">
         <img src={food} alt="" className="food-image"/>
-        <p><b>Meal Suggestion</b><br/>"Tailored Temptations: Satisfy Your Pet's Palate with 
+        <b>Meal Suggestion</b><p><br/>"Tailored Temptations: Satisfy Your Pet's Palate with 
          Personalized <br></br>Food Suggestions Catered to Their Unique Tastes and Preferences!"</p>
          <Link to="./meals"><button className="food-btn">Get Started <HiArrowCircleRight/></button></Link>
-         
       </div>
+      
       <div className="excerise-suggestion">
       <img src={ex} alt="" className="ex-image"/>
-      <p><b>Exercise Plans</b><br/>"Keep your pet active and joyful with our Exercise Planner! Personalized <br />workout routines designed 
+      <b>Exercise Plans</b><p><br/>"Keep your pet active and joyful with our Exercise Planner! Personalized <br />workout routines designed 
       for your furry friend's happiness and well-being. <br /> From playful games to easy walks, make every moment 
       together healthy and fun!"</p>
       <Link to="./exceisePlanner"><button className="ex-btn">Get Started <HiArrowCircleRight/></button></Link>
@@ -31,7 +31,7 @@ export const Services = () => {
       </div>
       <div className="consultant-search">
       <img src={petconsultant} alt="" className="consultant-image"/>
-        <p className="find-consultants-para"><b>Search for Consultants</b><br/>"Discover our new "Pet Consultant Search" feature! 
+      <b>Search for Consultants</b><p className="find-consultants-para"><br/>"Discover our new "Pet Consultant Search" feature! 
         Easily find and connect <br />with experienced pet consultants who can provide personalized care 
         tips <br />and guidance. Simply search, explore profiles, and book appointments for <br />tailored advice.
         Your pet's well-being is just a click away!"</p>
@@ -40,7 +40,7 @@ export const Services = () => {
       </div>
       <div className="vaccine-date">
       <img src={vaccine} alt="" className="vaccine-image"/>
-      <p><b>Vaccination reminder</b><br/>"Keep your pet's health in check effortlessly! Set personalized vaccination <br />
+      <b>Vaccination reminder</b><p><br/>"Keep your pet's health in check effortlessly! Set personalized vaccination <br />
        reminders, and we'll notify you on the exact date. Stay on top of your <br />furry 
        friend's well-being with automatic alerts and a handy vaccine <br /> history tracker. 
        Easy, convenient, and worry-free pet care at your fingertips!"</p>
@@ -49,7 +49,7 @@ export const Services = () => {
       </div>
       <div className="medical-data">
       <img src={medical} alt="" className="medical-image"/>
-      <p><b>Medical Data Store</b><br/>
+      <b>Medical Data Store</b><p><br/>
       "Safeguard your pet's health history with our "Pet Medical Records" feature. <br />
       Easily store and access vaccination details, treatments, and appointments <br />
       in one place. Keep track of your pet's well-being with this simple <br />
@@ -60,7 +60,7 @@ export const Services = () => {
 
       <div className="consultant-profile">
       <img src={consultant} alt="" className="consultant-profile-img"/>
-      <p><b>Bussiness Profiles for Consultants</b><br/>
+      <b>Bussiness Profiles for Consultants</b><p><br/>
       "Pet Consultants can create profile for them by inputing their<br /> Bussiness details,
        Then then the system save them and the Consultant <br />publishes the account then the pet oweners 
        can search for them!"
