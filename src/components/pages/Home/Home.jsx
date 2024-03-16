6423984b7784e204d396835c4cbe247f59381801
dileepaM-7 <dlpmalshan@gmail.com>
2024-03-16 11:35:57 +0530

    Update Home.jsx

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -48,7 +48,7 @@ export const Home = () => {
           <span className='welcome_txt'>Welcome... <br /><br /></span>
           <span className='first_content' >Welcome to a <br /> world of care for </span>
           <span className='second_content'>your <br />beloved pets <br /></span>
-          <span className='last_txt'>Discover expert pet care tips and advice to ensure your furry <br />
+          <span className='last_txt'>Hi Discover expert pet care tips and advice to ensure your furry <br />
             friend's well-being and happiness at every stage of their life.</span>
           <ScrollLink to="services" className='features-button' onClick={() => scrollToSection('services')}>
             Our Services <img src={play_Image} alt="" className="playImage" />
