b6d5c76a5fa0b0e2417a4fa84d5ca6aa3bd03a2c
dileepaM-7 <dlpmalshan@gmail.com>
2024-03-18 18:54:54 +0530

    Update Meals.jsx

--- src/components/pages/Meal-suggestion/Meals.jsx
+++ src/components/pages/Meal-suggestion/Meals.jsx
@@ -21,7 +21,7 @@ function Meals() {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://happypaws-ml.onrender.com/predict', {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json',
