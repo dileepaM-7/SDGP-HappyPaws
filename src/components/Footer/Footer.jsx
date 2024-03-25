800a2287e4588fb1c128b72357aa5dfddc200c1a
dileepaM-7 <dlpmalshan@gmail.com>
2024-03-25 09:59:19 +0530

    updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -34,21 +34,27 @@ const Footer = () => {
           
           <div className="sb_footer-links-div-services">
             <span className='FooterNewPart'>happy to welcome you <br/><span className='FooterNewPart2'> to Our Pet World</span></span>
+
             <img src={FooterIconImage} alt=""  className='FooterIconImage'/>
-            <span className='FooterService1'>Food Recommendations link</span>
+            <Link to="./meals"><span className='FooterService1'>Food Recommendations link</span></Link> 
             <p className='FooterService1-para1'>You can input your pets' breed, age,<br/> and weight to receive feeding suggestions<br/> for your adorable pets.</p>
+
+            
             <img src={FooterIconImage} alt=""  className='FooterIconImage'/>
-            <span className='FooterService1'>Vaccination reminder</span>
+            <Link to="./vaccine"><span className='FooterService1'>Vaccination reminder</span></Link>
             <p className='FooterService1-para1'>Reminders for vaccinations may help pet<br/> owners maintain their pets' health despite<br/> their busy lifestyles.</p>
+
             <img src={FooterIconImage} alt=""  className='FooterIconImage1'/>
-            <span className='FooterService3'>Exercise planner</span>
+            <Link to="./exceisePlanner"><span className='FooterService3'>Exercise planner</span></Link> 
             <p className='FooterService1-para2'>If you want get exercises for your pet buddies,<br/>here is the place.find your pets breed category<br/>and get exercises.</p>
+
             <img src={FooterIconImage} alt=""  className='FooterIconImage1'/>
-            <span className='FooterService3'>medical records</span>
+            <Link to="./medical"><span className='FooterService3'>Medical records</span></Link>
             <p className='FooterService1-para2'>Maintaining a pet’s medical record book is very<br/> important. keeping an online medical record for<br/> pets is easier than the traditional way.</p>
+
             
             <img src={FooterIconImage} alt=""  className='FooterIconImage3'/>
-            <span className='FooterService5'>Search for consultants</span>
+            <Link to="./consultant"><span className='FooterService5'>Search for consultants</span></Link>
             <p className='FooterService1-para5'>Pet owners may need assistance in finding<br/>  the veterinarian's contact information. We<br/>  allow visitors to access registered veterinarian<br/>  information via the website.</p>
    
 
